be4f4fdcc4cc7a96ba80fa052721aef03bf1f269
qtuancr261 <tuancr261@gmail.com>
2018-10-05 17:42:47 +0700

    Initialize check kch script on linux

---
+++ test_kch_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# script to run check kc hash database file
+dbName=$1
+partitionNum=$2
+echo "The test will backup kch db files from /data/apps/"$dbName"/db_data/"$dbName" to /backup/"$dbName
+# list all the db files
+ls -l /data/apps/$dbName/db_data/$dbName
+echo "Total partions: "$partitionNum
+# let's rsync
+mkdir -p /backup/$dbName
+rsync -av /data/apps/$dbName/db_data/* /backup/$dbName
+
+# do the check using kchashmgr
+#kchashmgr check 
+
+echo ".....Done......"
