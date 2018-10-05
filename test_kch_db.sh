ec3f2f47001a6477d984b06850361bafa8b3d9c8
qtuancr261 <tuancr261@gmail.com>
2018-10-05 18:30:57 +0700

    Added check -onr | More comments

--- test_kch_db.sh
+++ test_kch_db.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # script to run check kc hash database file
+# you need to install  kchashmgr for this test
+# kchashmgr is a Command line Utilities of Kytoto Cabinet to manage the file hash database
+# by tuantq3
 dbName=$1
 partitionNum=$2
 echo "The test will backup kch db files from /data/apps/"$dbName"/db_data/"$dbName" to /backup/"$dbName
@@ -19,6 +22,11 @@ do
     echo "#### Checking Partition " $index " ####"
     fileName=$dbName-$index
     kchashmgr inform -otl $fileName.kch
+    # check with try lock option
     kchashmgr check -otl $fileName.kch
+    # check with no lock option
+    kchashmgr check -onl $fileName.kch
+    # check with no repair option
+    kchashmgr check -onr $fileName.kch
 done
 echo ".....Done......"
