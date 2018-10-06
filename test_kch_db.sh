5c16f12185ac6ab0a1db6050139d724edeec4e6a
qtuancr261 <tuancr261@gmail.com>
2018-10-06 08:24:08 +0700

    Added more improvement 	- $userName : current user 	- $dbLocation : database location 	- $dbBackupLocation : database backup location 	- change owner of the $dbBackupLocation from root to $userName 	- More comment and echo

--- test_kch_db.sh
+++ test_kch_db.sh
@@ -3,8 +3,12 @@
 # you need to install  kchashmgr for this test
 # kchashmgr is a Command line Utilities of Kytoto Cabinet to manage the file hash database
 # by tuantq3
+userName=`id -u -n`
 dbName=$1
 partitionNum=$2
+dbLocation="/data/apps/"$dbName"/db_data/"$dbName
+dbBackupLocation="/backup/"$dbName
+echo $dbLocation " | " $dbBackupLocation 
 echo "The test will backup kch db files from /data/apps/"$dbName"/db_data/"$dbName" to /backup/"$dbName
 echo "-------------------------------------------------------------------------------------------------------------------------"
 # list all the db files
@@ -12,17 +16,20 @@ ls -l /data/apps/$dbName/db_data/$dbName
 echo "Total partions: "$partitionNum
 # let's rsync
 echo "-> making backup folder in /backup/"$dbName
-sudo mkdir -p /backup/$dbName
-#rsync -av /data/apps/$dbName/db_data/* /backup/$dbName
+sudo mkdir -p $dbBackupLocation
+sudo chown -R $userName:$userName $dbBackupLocation
+rsync -av $dbLocation/* $dbBackupLocation
 
 # do the check using kchashmgr
-echo "-> cd into /backup/"$dbName
-cd /backup/$dbName
+echo "-> cd into " $dbBackupLocation
+cd $dbBackupLocation
+pwd
 # kchashmgr check
 for ((index=0; index<$partitionNum; index++))
 do
     echo "---------Checking Partition " $index " ---------------"
     fileName=$dbName-$index
+    echo "Analyzing file: "$fileName.kch 
     kchashmgr inform -otl $fileName.kch
     # check with try lock option
     kchashmgr check -otl $fileName.kch
