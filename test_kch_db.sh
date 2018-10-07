a5d7fe1aeb59995977c76b2ae714a03ec14bb96c
qtuancr261 <tuancr261@gmail.com>
2018-10-08 00:14:55 +0700

    Merge

--- test_kch_db.sh
+++ test_kch_db.sh
@@ -3,24 +3,33 @@
 # you need to install  kchashmgr for this test
 # kchashmgr is a Command line Utilities of Kytoto Cabinet to manage the file hash database
 # by tuantq3
+userName=`id -u -n`
 dbName=$1
 partitionNum=$2
+dbLocation="/data/apps/"$dbName"/db_data/"$dbName
+dbBackupLocation="/backup/"$dbName
+
 echo "The test will backup kch db files from /data/apps/"$dbName"/db_data/"$dbName" to /backup/"$dbName
+echo "-------------------------------------------------------------------------------------------------------------------------"
 # list all the db files
 ls -l /data/apps/$dbName/db_data/$dbName
 echo "Total partions: "$partitionNum
 # let's rsync
-mkdir -p /backup/$dbName
-#rsync -av /data/apps/$dbName/db_data/* /backup/$dbName
+echo "-> making backup folder in /backup/"$dbName
+sudo mkdir -p $dbBackupLocation
+sudo chown -R $userName:$userName $dbBackupLocation
+rsync -av $dbLocation/* $dbBackupLocation
 
 # do the check using kchashmgr
-echo "cd into /backup/"$dbName
-cd /backup/$dbName
+echo "-> cd into " $dbBackupLocation
+cd $dbBackupLocation
+
 # kchashmgr check
 for ((index=0; index<$partitionNum; index++))
 do
-    echo "#### Checking Partition " $index " ####"
+    echo "---------Checking Partition " $index " ---------------"
     fileName=$dbName-$index
+    echo "Analyzing file: "$fileName.kch 
     kchashmgr inform -otl $fileName.kch
     # check with try lock option
     kchashmgr check -otl $fileName.kch
@@ -28,5 +37,7 @@ do
     kchashmgr check -onl $fileName.kch
     # check with no repair option
     kchashmgr check -onr $fileName.kch
+    echo "------------------------------------------------------"
 done
+
 echo ".....Done......"
