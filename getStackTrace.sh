96b9d57772f0f3ceedcfe40a6e60b2aac6e95b14
qtuancr261 <tuancr261@gmail.com>
2019-03-07 11:48:42 +0700

    Updated time trace

--- getStackTrace.sh
+++ getStackTrace.sh
@@ -3,7 +3,8 @@
 severPID=$1
 traceTime=$2
 flameGraphInfo=$3
+perfTraceTime=$(date +"%A_%d%m%Y_%H.%M.%S")
 perf record -F 99 -a -g -p $severPID sleep $traceTime
 perf script | ./stackcollapse-perf.pl > out.perf-folded
-./flamegraph.pl out.perf-folded > perf-kernel_31012019_2.svg
+./flamegraph.pl out.perf-folded > perf-kernel_$perfTraceTime.svg
 
