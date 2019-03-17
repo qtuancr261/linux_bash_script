42f33c4e824edd42af618e590251bb677801c1a8
qtuancr261 <tuancr261@gmail.com>
2019-03-17 21:33:13 +0700

    Added comments

--- eventbus_loadTest.sh
+++ eventbus_loadTest.sh
@@ -2,8 +2,12 @@
 # script to run an load test with eventbus
 # by tuantq3
 userName=`id -u -n`
+# zeventbusd
 serverName=$1
+# watcher_client 
 clientName=$2
+# b:watcher for listening
+# b:notifier_load for notifying
 clientOption_1=$3
 #clientOption_2=$4
 numInstances=$4
@@ -12,6 +16,5 @@ numInstances=$4
 for ((instanceIndex=1; instanceIndex<=$numInstances; ++instanceIndex))
 do
     echo "Run watcher instance "$instanceIndex
-    ./$clientName/bin_clone_$instanceIndex/$clientName"d" -$clientOption_1 &
-    #./$clientName/bin_58_4_clone_$instanceIndex/$clientName"d" -$clientOption_2 &
+    ./$clientName/bin_58_4_clone_$instanceIndex/$clientName"d" -$clientOption_1 &
 done
