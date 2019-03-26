5e2abe6eb66d3396819e66cf139cab55ec586f55
qtuancr261 <tuancr261@gmail.com>
2019-03-26 10:02:32 +0700

    Release 0.1

--- smartAutoRestartDB.sh
+++ smartAutoRestartDB.sh
@@ -7,62 +7,46 @@ dbType="strlist64bm"
 userHomeDir="/home/"$userName
 dbDataLocation="/data/apps/"$dbName"/commitlog/"$dbType
 dbBinLocation="/zserver/apps/dmp/DMPActiveUserListV2-2.2.24.214/bin"
-dbBackupLocation=$userHomeDir"/"$dbName
-dbArg="-r:cml "
-newestCmlDir=""
+dbBackupLocation=$userHomeDir"/"$dbName"_trace"
+lastestCmlDir=""
 
-#get newest cml folder
-cd $dbDataLocation
-for dir in */; do
-    if [ "$dir" \> "$newestCmlDir" ]; then
-        newestCmlDir=$dir
-    fi
+#check db status
+while true;
+do
+    while true;
+    do 
+        FINDAPP=`ps -x |grep $dbBinLocation | grep -v "grep"` 
+        echo $FINDAPP
+        if [[ $FINDAPP = "" ]]; then 
+            break
+        fi
+        sleep 1
+    done
+
+    echo "#####DB CRASH ######"    
+    autoRestartTime=$(date +"%A_%d%m%Y_%H.%M.%S")
+    mkdir -p $dbBackupLocation/$autoRestartTime
+    #get newest cml folder
+    cd $dbDataLocation
+    for dir in */; do
+        if [ "$dir" \> "$lastestCmlDir" ]; then
+            lastestCmlDir=$dir
+        fi
+    done
+    echo "Move : "$lastestCmlDir" to "$dbBackupLocation/$autoRestartTime
+    mv $lastestCmlDir $dbBackupLocation/$autoRestartTime
+    lastestCmlDir=""
+    for dir in */; do
+        if [ "$dir" \> "$lastestCmlDir" ]; then
+            lastestCmlDir=$dir
+        fi
+    done
+    echo "Backup "$lastestCmlDir" to "$dbBackupLocation/$autoRestartTime
+    rsync -rav $lastestCmlDir $dbBackupLocation/$autoRestartTime/$lastestCmlDir
+
+    cd $dbBinLocation
+    ./runservice start
+    echo "Restart completed......."
+    sleep 1
 done
-echo "Newest dir: "$newestCmlDir
-#check service status
-
-# while true;
-# do
-#     for dir in ./* do
-#         echo dir
-#     done
-# done
-
-# echo "The cript will backup newest dump file from /data/apps/"$dbName"/commitlog/"$dbName" to "$userHomeDir"/"$dbName
-# echo "And restart server"
-# echo "-------------------------------------------------------------------------------------------------------------------------"
-# # list cml
-# ls -al $dbDataLocation
-# # let's rsync
-# echo "-> making backup folder in "$userHomeDir/$dbName
-# mkdir -p $userHomeDir/$dbName
-# # get newest dump file and move it to 
-# cd $dbDataLocation
-# newestCmlDir=`ls -td -- */ | head -n 1`
-# echo $newestCmlDir
-# mv -v $newestCmlDir $dbBackupLocation
-# # restart server
-# cd $dbBinLocation
-# ./runservice start
-#########################################
-# # kchashmgr check
-# for ((index=0; index<$partitionNum; index++))
-# do  
-#     startTime=$(date +%M)
-#     echo "---------Checking Partition " $index " ---------------"
-#     fileName=$dbName-$index
-#     echo "------------------------------------------------------"
-#     endTime=$(date +%M)
-#     echo "START.$startTime.END.$endTime"
-# done
-
-# # Output the result test (or write to log)
-# totalTest=$((partitionNum*3))
-# writeLogTime=$(date +"%A_%d%m%Y_%H.%M.%S")
-# echo $writeLogTime " -> Passed Tests: " $passedTests "/" $totalTest >> "$dbName.checkLog.ini" 
-
-# # zip the kch file 
-# compressedFileName=$dbName"_"$writeLogTime-$passedTests"_"$totalTest.zip 
-# echo $compressedFileName
-# zip $compressedFileName $dbName-*
 # echo ".....Done......"
