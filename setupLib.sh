902b13cb2ad83b180f1de075cc2beb24a89afcea
qtuancr261 <tuancr261@gmail.com>
2020-04-09 09:58:21 +0700

    Added kate install

--- setupLib.sh
+++ setupLib.sh
@@ -16,7 +16,8 @@ sudo apt-get -y install g++ subversion git
 sudo apt install -y qt5-default libqt5charts5-dev qtmultimedia5-dev libqt5sensors5-dev
 # Qt document and examples
 sudo apt install -y qt5-doc-html qtmultimedia5-doc qtserialport5-doc qtserialport5-doc-html qtmultimedia5-examples qt5serialport-examples qtcharts5-examples qtsensors5-examples
-
+# KDE advanced text editor
+sudo apt install -y kate
 
 # Optinal for libboost
 sudo apt install -y libboost-all-dev
