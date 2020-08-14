71031bcd823df2d61c4e28e77a70a8c149bb545c
qtuancr261 <tuancr261@gmail.com>
2020-08-14 08:56:59 +0700

    <3 libcrypto++

--- setupLib.sh
+++ setupLib.sh
@@ -8,14 +8,14 @@ sudo apt-get -y install automake libtool cmake
 sudo apt-get -y install htop atop iotop dstat nethogs vim unrar rar p7zip-full uuid autoconf ssh
 sudo apt-get -y install libboost-all-dev flex unixodbc-dev mysql-client libmysqlclient-dev libevent-dev bison autoconf ssh sshfs samba libpcre++-dev libcurl3 libc6-dbg libnspr4 libnspr4-0d libnss3-1d libdb-dev libdb++-dev
 sudo apt-get -y install apache2 apache2-utils libapache2-mod-php5 php5 php5-dev php5-xdebug php-pear php5-cgi php5-cli php5-odbc php5-mysql
-sudo apt-get -y install libgtop2-dev openssl libssl-dev libssl1.0-dev
+sudo apt-get -y install libgtop2-dev openssl libssl-dev libssl1.0-dev libcrypto++-dev
 sudo apt-get -y install ttf-mscorefonts-installer openvpn lib32z1 lib32ncurses5 lib32bz2-1.0
 sudo apt-get -y install g++ subversion git
 
 # Optional for qtlib - Qt creatorqt5
 sudo apt install -y qt5-default libqt5charts5-dev qtmultimedia5-dev libqt5sensors5-dev
 # Qt document and examples
-sudo apt install -y qt5-doc-html qtmultimedia5-doc qtserialport5-doc qtserialport5-doc-html qtmultimedia5-examples qt5serialport-examples qtcharts5-examples qtsensors5-examples
+#sudo apt install -y qt5-doc-html qtmultimedia5-doc qtserialport5-doc qtserialport5-doc-html qtmultimedia5-examples qt5serialport-examples qtcharts5-examples qtsensors5-examples
 # KDE advanced text editor
 sudo apt install -y kate
 
@@ -23,10 +23,10 @@ sudo apt install -y kate
 sudo apt install -y libboost-all-dev
 
 # Optinal for snapd - snap apps
-sudo apt install -y snapd
-sudo apt install -y gnome-keyring
-sudo snap install mailspring
-sudo snap install kdictionary
+#sudo apt install -y snapd
+#sudo apt install -y gnome-keyring
+#sudo snap install mailspring
+#sudo snap install kdictionary
 
 # ibus
 sudo apt install -y ibus
