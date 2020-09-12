e0e555333e69c727db21d84a8be3943bd926e021
qtuancr261 <tuancr261@gmail.com>
2020-09-12 16:15:52 +0700

    Update some broken libs

--- setupLib_Ubuntu.sh
+++ setupLib_Ubuntu.sh
@@ -6,10 +6,9 @@ sudo apt-get -y install openvpn resolvconf
 # Build code utils
 sudo apt-get -y install automake libtool cmake
 sudo apt-get -y install htop atop iotop dstat nethogs vim unrar rar p7zip-full uuid autoconf ssh
-sudo apt-get -y install libboost-all-dev flex unixodbc-dev mysql-client libmysqlclient-dev libevent-dev bison autoconf ssh sshfs samba libpcre++-dev libcurl3 libc6-dbg libnspr4 libnspr4-0d libnss3-1d libdb-dev libdb++-dev
-sudo apt-get -y install apache2 apache2-utils libapache2-mod-php5 php5 php5-dev php5-xdebug php-pear php5-cgi php5-cli php5-odbc php5-mysql
-sudo apt-get -y install libgtop2-dev openssl libssl-dev libssl1.0-dev libcrypto++-dev
-sudo apt-get -y install ttf-mscorefonts-installer openvpn lib32z1 lib32ncurses5 lib32bz2-1.0
+sudo apt-get -y install libboost-all-dev flex unixodbc-dev libevent-dev bison autoconf ssh sshfs samba libpcre++-dev libc6-dbg libdb-dev libdb++-dev
+sudo apt-get -y install libgtop2-dev openssl libssl-dev libcrypto++-dev
+sudo apt-get -y install ttf-mscorefonts-installer lib32z1 lib32ncurses5
 sudo apt-get -y install g++ subversion git
 
 # Optional for qtlib - Qt creatorqt5
@@ -23,17 +22,13 @@ sudo apt-get -y install g++ subversion git
 sudo apt install -y libboost-all-dev
 
 # Optinal for snapd - snap apps
-#sudo apt install -y snapd
-#sudo apt install -y gnome-keyring
-#sudo snap install mailspring
-#sudo snap install kdictionary
 
 # ibus
 sudo apt install -y ibus
 im-config -n ibus
 sudo add-apt-repository ppa:bamboo-engine/ibus-bamboo
 sudo apt update
-
+sudo apt install -y ibus-bamboo
 
 # Optional for fix dualboot time
 read -p " => Do you want to use Local Time instead of UTC (Dualboot Time differences) Yes(y), No(N) ? : " RTC
@@ -54,5 +49,5 @@ KDE_ENV="/home/"$USER"/.config/plasma-workspace/env/"
 #	chmod a+x kwin.sh
 #	cd
 #else
-#	echo "tuantq3"
+#	echo "tuankjuT"
 #fi
